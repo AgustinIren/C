1104a5a556d9a7b59c1b852b4d4d9e452171fed9
MatiasFernandez <63112208+MatFernandez@users.noreply.github.com>
2022-04-09 02:04:58 -0300

    corrigo commits

--- archivosPrincipales/main.c
+++ archivosPrincipales/main.c
@@ -6,19 +6,9 @@ int main(){
 
 	printf("Hello world!");
 
-<<<<<<< HEAD
-
-
-
-
-
-
 	printf("otro commit");
-
-
-=======
 	printf("Hola");
->>>>>>> 104db2961294a4ef473efa85e1d41cbebce3f1b3
+
 	return 0;
 
 
