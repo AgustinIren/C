c4ea003311744c95d175e8ed84be13a1635e308a
MatFernandez <mattifernan19@gmail.com>
2022-04-09 02:13:45 -0300

    corrijo todo

--- archivosPrincipales/main.c
+++ archivosPrincipales/main.c
@@ -3,12 +3,15 @@
 
 int main(){
 
+	int edad;
 
-	printf("Hello world!");
+	printf("Hello world!\n");
 
-	printf("otro commit");
-	printf("Hola");
-
+	printf("otro commit\n");
+	printf("Hola\n");
+	printf("Ingrese edad\n");
+	scanf("%i", &edad);
+	printf("La edad ingresada es de %i \n", edad);
 	return 0;
 
 
